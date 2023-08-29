1eefec1cbee984a9298a331ad84d53c86e4490b6
Magnus Madsen <magn5310@stud.kea.dk>
2023-08-29 10:37:39 +0200

    gmn

--- test.js
+++ test.js
@@ -1,26 +1,76 @@
-"use strict";
 
-console.log(Math.floor(Math.random() * 100));
+// console.log(Math.floor(Math.random() * 100));
 
-// Generate a Random Number
-let y = Math.floor(Math.random() * 100 + 1);
+// // Generate a Random Number
+// let y = Math.floor(Math.random() * 100 + 1);
 
-// Counting the number of guesses
-// made for correct Guess
-let guess = 1;
+// // Counting the number of guesses
+// // made for correct Guess
+// let guess = 1;
 
-document.getElementById("submitguess").onclick = function () {
-  // Number guessed by user
-  let x = document.getElementById("guessField").value;
+// document.getElementById("submitguess").onclick = function () {
+//   // Number guessed by user
+//   let x = document.getElementById("guessField").value;
 
-  if (x == y) {
-    console.log("Korrekt, du brugte " + guess + " forsøg ");
-  } else if (x > y) {
-    /* If guessed number is greater than actual number*/
-    guess++;
-    console.log("Mindre");
-  } else {
-    guess++;
-    console.log("Højere");
-  }
-};
+//   if (x == y) {
+//     console.log("Korrekt, du brugte " + guess + " forsøg ");
+//   } else if (x > y) {
+//     /* If guessed number is greater than actual number*/
+//     guess++;
+//     console.log("Mindre");
+//   } else {
+//     guess++;
+//     console.log("Højere");
+//   }
+// };
+
+let gæt;
+let antal =0;
+let min=0;
+let max=100;
+const startGame = document.querySelector(".startgame");
+const rigtig = document.querySelector(".rigtig");
+const mindre = document.querySelector(".mindre");
+const højere = document.querySelector(".højere");
+const comGuess = document.querySelector(".comguess");
+startGame.addEventListener("click", start);
+
+function guess(){
+  gæt = Math.floor((min + max) / 2);
+  comGuess.textContent = "Jeg gætter på"+" "+gæt;
+  antal++;
+}
+
+function start(){
+  min=0;
+  max=100;
+ rigtig.addEventListener("click", korrekt);
+ mindre.addEventListener("click", lavere);
+ højere.addEventListener("click", mere);
+
+ antal =0;
+ startGame.textContent="Genstart"
+ guess();
+
+}
+
+function lavere(){
+  max = gæt;
+  guess();
+
+}
+
+function mere(){
+  min = gæt+1;
+  guess();
+}
+
+function korrekt(){
+comGuess.textContent="Yes, jeg gættede dit tal"+" "+gæt+" "+"på"+" "+antal+" "+"forsøg";
+rigtig.removeEventListener("click", korrekt);
+mindre.removeEventListener("click", lavere);
+højere.removeEventListener("click", mere);
+startGame.addEventListener("click", start);
+}
+
+
